49d01b3c80dabee25e9a81fa3081b042c45d413c
David Strawn <david.strawn@banno.com>
2022-01-28 07:59:29 -0700

    squash!

--- core/src/main/scala/cats/uri/PercentDecoder.scala
+++ core/src/main/scala/cats/uri/PercentDecoder.scala
@@ -131,148 +131,180 @@ object PercentDecoder {
     val len: Int = value.length
     val buffer: IntBuffer = IntBuffer.allocate(value.codePointCount(0, len))
     val miniBuffer: IntBuffer = IntBuffer.allocate(3)
+    var error: String = null
+    var index: Int = 0
 
-    def decodeByte1(a: Int): Either[String, Int] =
+    def setError(e: String): Unit =
+      if (error eq null) {
+        error = e
+      } else {
+        ()
+      }
+
+    def decodeByte1(a: Int): Int =
       if ((a & 0x80) == 0) {
-        Right(a.toInt)
+        a.toInt
       } else {
-        Left(s"Invalid UTF-8 byte encoding. Characters of represented by 1 byte must by < 0x80: ${a}")
+        setError(s"Error at index ${index - 3}. Invalid UTF-8 byte encoding: ${a.toHexString}")
+        Int.MinValue
       }
 
-    def decodeByte2(a: Int, b: Int): Either[String, Int] =
+    def decodeByte2(a: Int, b: Int): Int =
       if ((a & 0xe0) == 0xc0 && (b & 0xc0) == 0x80) {
-        Right((a & 0x1f) << 6 | b & 0x3f)
+        (a & 0x1f) << 6 | b & 0x3f
       } else {
-        Left(s"Invalid UTF-8 byte encoding. For characters represented by 2 bytes, the first byte must be < 0xe0 and the second by must be < 0xc0: ${a} ${b}")
+        setError(s"Error at index ${index - 6}. Invalid UTF-8 byte encoding: ${a.toHexString} ${b.toHexString}")
+        Int.MinValue
       }
 
-    def decodeByte3(a: Int, b: Int, c: Int): Either[String, Int] =
+    def decodeByte3(a: Int, b: Int, c: Int): Int =
       if ((a & 0xf0) == 0xe0 && (b & 0xc0) == 0x80 && (c & 0xc0) == 0x80) {
-        Right((a & 0xf) << 12 | (b & 0x3f) << 6 | c & 0x3f)
+        (a & 0xf) << 12 | (b & 0x3f) << 6 | c & 0x3f
       } else {
-        Left(s"Invalid UTF-8 byte encoding. For characters represented by 3 bytes, the first byte must be < 0xf0 and the second and third by must be < 0xc0: ${a} ${b} ${c}")
+        setError(s"Error at index ${index - 9}. Invalid UTF-8 byte encoding: ${a.toHexString} ${b.toHexString} ${c.toHexString}")
+        Int.MinValue
       }
 
-    def decodeByte4(a: Int, b: Int, c: Int, d: Int): Either[String, Int] =
+    def decodeByte4(a: Int, b: Int, c: Int, d: Int): Int =
       if ((a & 0xf8) == 0xf0 && (b & 0xc0) == 0x80 && (c & 0xc0) == 0x80 && (d & 0xc0) == 0x80) {
-        Right((a & 0x7) << 18 | (b & 0x3f) << 12 | (c & 0x3f) << 6 | d & 0x3f)
+        (a & 0x7) << 18 | (b & 0x3f) << 12 | (c & 0x3f) << 6 | d & 0x3f
       } else {
-        Left(s"Invalid UTF-8 byte encoding. For characters represented by 4 bytes, the first byte must be < 0xf8 and the second and third by must be < 0xc0: ${a} ${b} ${c}")
+        setError(s"Erorr at index ${index - 12}. Invalid UTF-8 byte encoding: ${a.toHexString} ${b.toHexString} ${c.toHexString}")
+        Int.MinValue
       }
 
-    def extractPercentHexBytes(
-      count: Int,
-      cursor: Int
-    ): Either[String, Unit] = {
-      if (count <= 0) {
-        Right(())
-      } else if (cursor + 2 >= len) {
-        Left(s"Encountered an incomplete percent encoded UTF-8 byte sequence at index ${cursor - 3} of the given String.")
+    def utf8ByteLength(b: Int): Int =
+      if ((b & 0x80) == 0) {
+        1
+      } else if ((b & 0xe0) == 0xc0) {
+        2
+      } else if ((b & 0xf0) == 0xe0) {
+        3
+      } else if ((b & 0xf8) == 0xf0) {
+        4
       } else {
-        value.codePointAt(cursor) match {
-          case PercentUnicodeCodePoint =>
-            value.codePointAt(cursor + 1) match {
-              case hi if hi <= 'z'.toInt =>
-                val low: Int = value.codePointAt(cursor + 2)
-                hexCodePointsToByte(hi, low) match {
-                  case Right(byte) =>
-                    miniBuffer.put(byte)
-                    extractPercentHexBytes(count - 1, cursor + 3)
-                  case Left(e) =>
-                    Left(e)
-                }
-              case otherwise =>
-                Left(s"Encountered an invalid percent encoded sequence at index ${cursor}. Codepoint ${otherwise} does not represent a valid hexidecimal character.")
-            }
-          case otherwise =>
-            Left(s"Expected '%' when decoding multibyte UTF-8 byte sequence at index ${cursor - 3}, got codepoint ${otherwise.toHexString}.")
+        setError(s"Error encountered at index ${index - 3}. Found a percent encoded sequence but it does not corrispond to any valud UTF-8 leading byte.")
+        Int.MinValue
+      }
+
+    def hexCodePointToInt(codePoint: Int): Int =
+      codePoint match {
+        case c if c >= '0' && c <= '9' => (c - '0')
+        case c if c >= 'A' && c <= 'F' => 10 + (c - 'A')
+        case c if c >= 'a' && c <= 'f' => 10 + (c - 'a')
+        case _ =>
+          setError(s"Error encountered at ${index}. Character represented by unicode codepoint ${codePoint} is not a valid hex character.")
+          Int.MinValue
+      }
+
+    // Read two codePoints and handle the index bookeeping.
+    def readNextTwoCodePointsAsHexByte: Int = {
+      if (index + 1 < len) {
+        val hi: Int = hexCodePointToInt(value.codePointAt(index))
+        val low: Int = hexCodePointToInt(value.codePointAt(index + 1))
+        if (error eq null) {
+          index += 2
+          ((hi << 4) | low)
+        } else {
+          Int.MinValue
         }
+      } else {
+        setError(s"Error encountered at ${index}. Expected at least two more characters to decode percent encoded value, but only found ${index - len}")
+        Int.MinValue
       }
     }
 
-    def extractPercentHexByteSequence(
-      cursor: Int
-    ): Either[String, Int] =
-      value.codePointAt(cursor) match {
-        case hi if hi <= 'z'.toInt =>
-          val low: Int = value.codePointAt(cursor + 1)
-          hexCodePointsToByte(hi, low) match {
-            case Right(byte) =>
-              utf8ByteLength(byte) match {
-                case Right(1) =>
-                  decodeByte1(byte) match {
-                    case Right(codePoint) =>
-                      buffer.put(codePoint)
-                      Right(cursor + 2)
-                    case Left(e) => Left(e)
-                  }
-                case Right(2) =>
-                  miniBuffer.clear()
-                  extractPercentHexBytes(1, cursor + 2)
-                  decodeByte2(byte, miniBuffer.get(0)) match {
-                    case Right(codePoint) =>
-                      buffer.put(codePoint)
-                      Right(cursor + 2 + 3)
-                    case Left(e) => Left(e)
-                  }
-                case Right(3) =>
-                  miniBuffer.clear()
-                  extractPercentHexBytes(2, cursor + 2)
-                  decodeByte3(byte, miniBuffer.get(0), miniBuffer.get(1)) match {
-                    case Right(codePoint) =>
-                      buffer.put(codePoint)
-                      Right(cursor + 2 + 6)
-                    case Left(e) => Left(e)
-                  }
-                case Right(4) =>
-                  miniBuffer.clear()
-                  extractPercentHexBytes(3, cursor + 3)
-                  decodeByte4(byte, miniBuffer.get(0), miniBuffer.get(1), miniBuffer.get(2)) match {
-                    case Right(codePoint) =>
-                      buffer.put(codePoint)
-                      Right(cursor + 2 + 9)
-                    case Left(e) => Left(e)
-                  }
-                case Left(e) =>
-                  Left(e)
-                case Right(invalid) =>
-                  throw new AssertionError(s"Supposed UTF-8 leading byte ${byte.toHexString} indicated a byte length for the UTF-8 codepoint of ${invalid}, but that is not possible. This is a cats-uri bug.")
-              }
-            case Left(e) => Left(e)
-          }
-        case otherwise =>
-          Left(s"Encountered an invalid percent encoded sequence at index ${cursor}. Codepoint ${otherwise} does not represent a valid hexidecimal character.")
+    def decodePercentByte: Int =
+      if (index + 2 < len) {
+        value.codePointAt(index) match {
+          case PercentUnicodeCodePoint =>
+            index += 1
+            readNextTwoCodePointsAsHexByte
+          case otherwise =>
+            setError(s"Error encountered at ${index}. Expected at least one more percent encoded UTF-8 byte sequence, but next codepoint was not a '%': ${otherwise}")
+            Int.MinValue
+        }
+      } else {
+        setError(s"Error encountered at ${index}. Expected at least one more percent encoded UTF-8 byte sequence, but reached the end of the String.")
+        Int.MinValue
       }
 
-    @tailrec
-    def loop(index: Int): Either[String, String] =
-      if (index >= len) {
-        val count: Int = buffer.position()
-        buffer.flip
-        Right(new String(buffer.array, 0, count))
-      } else {
-        val codePoint: Int = value.codePointAt(index)
-
-        codePoint match {
-          case PercentUnicodeCodePoint =>
-            if (index + 2 >= len) {
-              Left(s"Encountered unterminated % sequence at index ${index} of the String. Expected a % followed by two hexidecimal digits.")
+    def extractPercentHexByteSequence: Unit = {
+      val a: Int = readNextTwoCodePointsAsHexByte
+      if (error eq null) {
+        utf8ByteLength(a) match {
+          case 1 =>
+            val codePoint: Int = decodeByte1(a)
+            if (error eq null) {
+              buffer.put(codePoint)
             } else {
-              extractPercentHexByteSequence(index + 1) match {
-                case Right(next) =>
-                  loop(next)
-                case Left(e) =>
-                  Left(e)
-              }
+              ()
             }
-          case rawCodePoint =>
-            buffer.put(rawCodePoint)
-            val inc: Int = if (rawCodePoint >= 0x10000) 2 else 1
-            loop(index + inc)
+          case 2 =>
+            val codePoint: Int = decodeByte2(a, decodePercentByte)
+            if (error eq null) {
+              buffer.put(codePoint)
+            } else {
+              ()
+            }
+          case 3 =>
+            val codePoint: Int = decodeByte3(a, decodePercentByte, decodePercentByte)
+            if (error eq null) {
+              buffer.put(codePoint)
+            } else {
+              ()
+            }
+          case 4 =>
+            val codePoint: Int = decodeByte4(a, decodePercentByte, decodePercentByte, decodePercentByte)
+            if (error eq null) {
+              buffer.put(codePoint)
+            } else {
+              ()
+            }
+          case _ =>
+            // Should already be set
+            setError(s"Error encountered at index ${index - 3}. Found a percent encoded sequence but it does not corrispond to any valud UTF-8 leading byte.")
+        }
+      } else {
+        ()
+      }
+    }
+
+    while(index < len && (error eq null)) {
+      value.codePointAt(index) match {
+        case PercentUnicodeCodePoint =>
+          // '%' is always only 1 character in the String
+          index += 1
+
+          // Check to ensure there are at least 2 more character for the
+          // expected 2 hexidecimal character.
+          if (index + 1 >= len) {
+            error = s"Encountered unterminated % sequence at index ${index} of the String. Expected a % followed by two hexidecimal digits."
+          } else {
+            // Index bookeeping is handled in this function
+            extractPercentHexByteSequence
+          }
+        case codePoint =>
+          // Does not need decoding, goes directly into the buffer.
+          buffer.put(codePoint)
+
+          // Unicode codepoints >= 0x10000 are represnted by two characters in
+          // the String, (value: String).codePointAt(index) accounts for this,
+          // but we need to increment our index by 2 in this case. Otherwise
+          // we would read the second character of a surrogate pair, which is
+          // not what we want.
+          val inc: Int = if (codePoint >= 0x10000) 2 else 1
+          index += inc
         }
       }
 
-    loop(0)
+    if (error eq null) {
+      val count: Int = buffer.position()
+      buffer.flip
+      Right(new String(buffer.array, 0, count))
+    } else {
+      Left(error)
+    }
   }
   // (percentByteSubStringParser | (Parser.anyChar *> Parser.charsWhile0(_ != '%')).string).repAs0[String].parseAll(value).fold(
   //   e => Left(s"Decoding the percent encoded value failed. This likely means there was an invalid percent encoded byte sequence or you have a literal '%' in the String which does not map to a percent encoded UTF-8 byte sequence"),
